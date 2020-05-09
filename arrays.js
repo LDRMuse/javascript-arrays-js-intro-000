2eab7df2d7b7c8dcf83376da4e0a4ef149ed72a9
Melissa Heying <melissakheying@gmail.com>
2020-05-09 16:51:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,11 @@ function addElementToBeginningOfArray(array, element) {
   console.log([1], 'foo')
 }
 
-
+let array = 1;
+let element = 'foo'
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  console.log([1], 'foo')
+}
 /*  describe('addElementToBeginningOfArray(array, element)', () => {
     it('adds an element to the beginning of an array', () => {
       expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
