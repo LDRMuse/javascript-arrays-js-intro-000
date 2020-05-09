872a15f79ed8c456bea431f7abe332f450a02463
Melissa Heying <melissakheying@gmail.com>
2020-05-09 17:31:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,6 @@
-var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-
-const array = [1];
-let element = 'foo'
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
-}
-
-function addElementToEndOfArray(array, element) {
   return [element, ...array]
 }
 
@@ -16,7 +9,33 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
 
 function accessElementInArray(array, index) {
-  return array[index];
+  return array[index]
 }
