507869dd4b5b5db68cfc1793399690dd24ae37cd
Melissa Heying <melissakheying@gmail.com>
2020-05-09 17:25:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,25 +7,16 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function addElementToEndOfArray(array, element) {
-  return [...array, element]
+
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
-}
 
 
 function accessElementInArray(array, index) {
   return array[index];
 }
-/*  describe('accessElementInArray(array, index)', () => {
-    it('accesses the element in array at the given index', () => {
-      expect(accessElementInArray([1, 2, 3], 2)).to.equal(3)
-    })
-  })*/
+
