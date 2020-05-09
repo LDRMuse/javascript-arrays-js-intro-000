5593fd076a6ba713bd23e4926ebef3c77d5e189a
Melissa Heying <melissakheying@gmail.com>
2020-05-09 16:48:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,14 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ];
+
+
+let array = 1;
+let element = 'foo'
+function addElementToBeginningOfArray(array, element) {
+  console.log([1], 'foo')
+}
+
+
+/*  describe('addElementToBeginningOfArray(array, element)', () => {
+    it('adds an element to the beginning of an array', () => {
+      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+    })*/
